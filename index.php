1271b49499bf7da9ed0ba9e9bf3a14a7aa514c90
Christina Andersen <webdesign@art-visual.ch>
2013-06-20 17:38:39 +0200

    HTML änderung

--- index.php
+++ index.php
@@ -16,34 +16,34 @@ $page = $site->page();
 $orgs = EBSite::organisations();
 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
+<!DOCTYPE html>
+<html lang="de-CH">
 <head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta charset="UTF-8">
 <title><?php echo $site->title; ?></title>
 <link rel="stylesheet" media="all" type="text/css" href="css/style.css">
-<link rel="stylesheet" media="print" type="text/css" href="css/print.css" />
+<link rel="stylesheet" media="print" type="text/css" href="css/print.css">
 <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
 </head>
 <body>
 <div id="header" role="banner">
-  <h1><a href="index.php"><?php echo $site->title; ?></a></h1>
-  <?php echo $page->header; ?> </div>
+<h1><a href="index.php"><?php echo $site->title; ?></a></h1>
+<?php echo $page->header; ?> </div>
 <div id="left">
-  <?php
+<?php
       include EB_DATA.'intro.html';
       echo $orgs->linklist();
       ?>
 </div>
 <div id="content" role="main">
-  <?php
+<?php
       if ($title = $page->title) echo '<h2>'.$title.'</h2>';
       echo $page->message;
       echo $page->content;
       ?>
 </div>
 <div id="footer" role="contentinfo">
-  <?php
+<?php
       echo $page->footer;
       include EB_DATA.'footer.html';
       ?>
